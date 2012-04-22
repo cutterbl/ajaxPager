750f709c5224ab942ddf62a5c4aa97b8715c913b
Steve 'Cutter' Blades <no.junk@cutterscrossing.com>
2012-04-22 15:12:08 -0400

    Initial commit of files to the project

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-ajaxPager
-=========
+The ajaxPager is a JQuery + Bootstrap plugin that creates a pagination
+control bar, according to configuration, that will make remote requests
+for data. The control then passes the response to a developer defined
+method to create the output. In the example, we used the Handlebars
+templating engine to create display items of each record in a returned
+JSON recordset.
 
-JQuery + Bootstrap plugin to create paging controls that handle making remote requests for data. More details in the readme
+The ajaxPager requires two files, the script and the stylesheet. The
+developer is then responsible for handling the formatting of the response
+output. This initial release is a beta, and prior to any documentation.
---
+++ resources/scripts/custom/pagingTest.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+	var tplContent = $('#line-template').html();
+	var lineTpl = Handlebars.compile(tplContent);
+	
+	var convertResponse = function (data){
+		return $.serializeCFJSON(data);
+	}
+	
+	var createOutput = function(data) {
+		return lineTpl(data);
+	}
+	
+	var testHandler = function (){
+		console.log(arguments);
+	}
+	
+	$('div#testCont').ajaxPager({
+		position:'both',
+		limit: 10,
+		ajaxoptions: {
+			url: '/com/cc/Blog/Entries.cfc',
+			data: {
+				method: 'getEntries',
+				returnFormat: 'json'
+			},
+			dataType: 'json'
+		},
+		params: {
+			page: 'pageIndex',
+			limit: 'pageSize',
+			sort: 'sortCol',
+			dir: 'sortDir'
+		},
+		reader: {
+			totalpages: 'pageCount',
+			totalrecords: 'recordCount',
+			root: 'data'
+		},
+		listeners: {
+			init: testHandler,
+			render: testHandler,
+			preprocess: convertResponse,
+			beforeload	: testHandler,
+			load		: testHandler,
+			destroy		: testHandler
+		},
+		sortcolumn: 'title',
+		filters: {
+			title: 'Title',
+			posted: 'Date Posted',
+			views: 'Views'
+		},
+		//searchtext: '{"title":"ColdFusion"}',
+		renderoutput: createOutput,
+		stripedrows: true
+	});
+	
+});
