6bb971604cdf03e24a46e645d25eb42664093dd8
Steve 'Cutter' Blades <no.junk@cutterscrossing.com>
2014-03-03 10:50:02 -0600

    Update bootstrap.ajaxpager-0.8.js
    Add license

--- resources/scripts/bootstrap.ajaxpager-0.8.js
+++ resources/scripts/bootstrap.ajaxpager-0.8.js
@@ -1,8 +1,24 @@
-/**
- * @depends /core/jquery-1.7.2.js
- * @depends /bootstrap/bootstrap.js
- * @depends /bootstrap/html5.js
- */
+/* =============================================================
+ * bootstrap.ajaxpager.js v0.8
+ * http://twitter.github.com/cutterbl/ajaxPager
+ * 
+ * Inspired by Twitter Bootstrap, with credit to bits of code
+ * from all over.
+ * =============================================================
+ * Copyright 2010 - 2015 Cutters Crossing.
+ *
+ * Licensed under the GNU General Public License v2
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ ** http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============================================================ */
 (function($) {
 
 	$.fn.ajaxPager = function(options) {
